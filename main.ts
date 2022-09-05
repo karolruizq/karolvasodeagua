da94afb624252148fd0a43897d0df17fc31b182d
karolruizq <110991486+karolruizq@users.noreply.github.com>
2022-09-05 16:49:20 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,74 @@
+// vaso llenandose de agua
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . # . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . # . #
+        # . # . #
+        # . # . #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . # . #
+        # . # . #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # . # . #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # . . # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(150)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+}
